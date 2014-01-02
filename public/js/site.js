2926d025199d9646978f1c24f3d99c0662d8956d
Conar Welsh <conar@sellwelldesigns.com>
2014-01-02 13:57:32 -0700

    initial commit

---
+++ public/js/site.js
@@ -0,0 +1,12 @@
+//doc ready
+$(function(){
+
+    var
+        $window = $(window),
+        $document = $(document),
+        $body = $('body')
+    ;
+
+    stroll.bind('#content');
+    
+});//end doc ready
